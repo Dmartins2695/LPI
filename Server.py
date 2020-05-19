e187862aa0f2c151633d1e8a37e21b1121af3661
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2020-05-19 11:00:26 +0100

    Comunication with server working
    Database creating

--- Server.py
+++ Server.py
@@ -1,78 +1,144 @@
-import socket
-import sys
+from flask import Flask,render_template,request
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import cv2
-import pickle
-import numpy as np
-import struct ## new
-import zlib
-
-HOST=''
-PORT=8485
-
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print('Socket created')
-
-s.bind((HOST,PORT))
-print('Socket bind complete')
-s.listen(10)
-print('Socket now listening')
-
-conn,addr=s.accept()
-
-data = b""
-payload_size = struct.calcsize(">L")
-print("payload_size: {}".format(payload_size))
-while True:
-    while len(data) < payload_size:
-        print("Recv: {}".format(len(data)))
-        data += conn.recv(4096)
-
-    print("Done Recv: {}".format(len(data)))
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack(">L", packed_msg_size)[0]
-    print("msg_size: {}".format(msg_size))
-    while len(data) < msg_size:
-        data += conn.recv(4096)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-
-    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-    cv2.imshow('ImageWindow',frame)
-<<<<<<< HEAD
-    cv2.waitKey(1)
-=======
-    cv2.waitKey(1)
-
-    def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-
-    conn.close()
-        
-
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+import base64
+import os, sqlalchemy
+from passlib.hash import pbkdf2_sha256 as sha256
 
 
-print("[STARTING] server is starting...")
-start()
->>>>>>> 460b2acbec2c0b871b49dcbb03380f82b40831f0
+app=Flask(__name__)
+api=Api(app)
+parser = reqparse.RequestParser()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Lolada123@localhost/dbLPI'
+db = SQLAlchemy(app)
+
+
+# cria todas as tabelas
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+#@jwt.token_in_blacklist_loader
+#def check_if_token_in_blacklist(decrypted_token):
+   # jti = decrypted_token['jti']
+    #return RevokedTokenModel.is_jti_blacklisted(jti)
+
+# TABELAS
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    #email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # verifica se ja existe algum utilizador com esse username
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+   #@classmethod
+   # def find_by_email(cls, email):
+      #  return cls.query.filter_by(email=email).first()
+
+    @classmethod
+    def find_images_by_id(cls, id):
+        return ImageModel.find_images_by_id(id)
+
+    # retorna todos os utilizadores
+    @classmethod
+    def return_all(cls):
+        def to_json(x):
+            return {
+                'username': x.username,
+                'password': x.password
+            }
+
+        return {'users': list(map(lambda x: to_json(x), UserModel.query.all()))}
+
+    # elimina todos os utilizadores
+    @classmethod
+    def delete_all(cls):
+        try:
+            num_rows_deleted = db.session.query(cls).delete()
+            db.session.commit()
+            return {'message': '{} row(s) deleted'.format(num_rows_deleted)}
+        except:
+            return {'message': 'Something went wrong'}
+
+    # para gerar o hash
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+
+    # para verificar hash no login
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
+
+# class UserLogin(Resource):
+#     def post(self):
+#         parser_login = parser.copy()
+#         parser_login.add_argument('username', help='This field cannot be blank', required=False)
+#         parser_login.add_argument('password', help='This field cannot be blank', required=False)
+#         data = parser_login.parse_args()
+#         current_user = UserModel.find_by_username(data['username'])
+#         if not current_user:
+#             return {
+#                 'message': 'Wrong credentials',
+#                 'access_token':"error",
+#                 'code': 'user_not_exists'
+#             }
+#
+#         if UserModel.verify_hash(data['password'], current_user.password):
+#             access_token = create_access_token(identity=data['username'])
+#             refresh_token = create_refresh_token(identity = data['username'])
+#             return {
+#                 'message': 'Logged in as {}'.format(current_user.username),
+#                 'access_token': access_token,
+#                 'refresh_token': refresh_token,
+#                 'code': 'login_with_fields'
+#             }
+#         else:
+#             return {
+#                 'message': 'Wrong credentials',
+#                 'access_token' : "error",
+#                 'code': 'wrong_credentials'
+#             }
+
+@app.route('/student/<id>')
+def hello_world(id):
+    return 'Hello,'+id
+
+class receiveImage(Resource):
+    def post(self):
+        path="C:\\Users\plato\PycharmProjects\Client\\venv\App\serverImages"
+        parser_upload = parser.copy()
+        parser_upload.add_argument('image', help='Image cannot be blank', required=False)
+        data = parser_upload.parse_args()
+        image=data['image']
+        bytes=base64.b64decode(image)
+        date = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        with open(path+"\\"+date+'.jpg', "wb") as img:
+             img.write(bytes)
+             print(img.name)
+        return 200
+
+
+
+
+api.add_resource(receiveImage, '/receiveImage', endpoint="receiveImage")
+
+
+
+if __name__  == '__main__':
+    db.create_all()
+    app.run(debug=True,host="192.168.1.99", port="5000")
