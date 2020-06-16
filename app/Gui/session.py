5e4686d5c099ae08ba6221e714204dca2fe7ff1f
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2020-06-16 17:11:23 +0100

    update

--- app/Gui/session.py
+++ app/Gui/session.py
@@ -9,13 +9,15 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
 from PyQt5.uic import loadUi
 
-URL = "http://192.168.1.134:5000"
 
-credentials=[]
+URL = "http://192.168.1.99:5000"
+
+credentials = []
 
 class MainPage(QMainWindow):
     def __init__(self):
         super(MainPage, self).__init__()
+        self.myotherWindow = CodePage()
         loadUi('form.ui', self)
         self.btn_login.clicked.connect(self.login)
         self.btn_reg.clicked.connect(self.register)
@@ -26,7 +28,7 @@ class MainPage(QMainWindow):
         json = {'StudentUser': username, 'StudentPassword': password}
         print(json)
         postRequest = requests.post(url=URL + '/studentLogin', data=json)
-        postJason= postRequest.json()
+        postJason = postRequest.json()
 
         if postJason['code'] == 'wrong_credentials':
             print('erro login')
@@ -34,6 +36,8 @@ class MainPage(QMainWindow):
             self.login_username.clear()
             self.login_pass.clear()
             credentials.append(username)
+            widget.close()
+            self.myotherWindow.show()
             print('login com sucesso')
 
     def register(self):
@@ -50,6 +54,27 @@ class MainPage(QMainWindow):
             self.reg_email.clear()
         else:
             print('Erro no registro')
+
+
+class CodePage(QMainWindow):
+    def __init__(self):
+        super(CodePage, self).__init__()
+        loadUi('codeForm.ui', self)
+        self.btn1.clicked.connect(self.getCode)
+
+    def getCode(self):
+        roomCode = self.code.text()
+        print(roomCode)
+        json = {'roomCode': roomCode}
+        postRequest = requests.post(url=URL + '/receiveCode', data=json)
+        postJason = postRequest.json()
+        if postJason['code'] == 'sucess':
+            self.code.clear()
+            print('enter room')
+        elif postJason['code'] == 'error':
+            self.code.clear()
+            print(postJason['message'])
+
 
 
 app = QApplication(sys.argv)
